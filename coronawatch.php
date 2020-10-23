dc0819086763403d8c89865732c616acb8d1d25e
Aydar <contact@fetchdev.host>
2020-10-23 12:00:14 +0300

    fixed one dumb bug

--- coronawatch.php
+++ coronawatch.php
@@ -9,10 +9,10 @@ $today = $data['countrydata'][0]['total_new_cases_today'];
 if($today == 0){
         echo ("ERR: No data for today yet");
 } else {
-        $def = array("today" => $today, "record" => ($cache['today'] < $today) ? $today : $cache['today'], "date" => date("m/d/y"));
+        $def = array("today" => $today, "record" => ($cache['record'] > $today) ? $today : $cache['record'], "date" => date("m/d/y"));
 
         if(in_array('-t', $argv)){
-                $ret = '\\[CW\\]: *\\+' . $today . '* \\> *' . ($today - $cache['today']) . '*, R: *' . $cache['record'] . '*';
+                $ret = '\[CW\]: *\\+' . $today . '* \\> *' . ($today - $cache['today']) . '*, R: *' . $cache['record'] . '*';
                 shell_exec('telegrambotreport -m "' . $ret . '"');
         } else {
                 $ret = '+' . $today . ' (' . ($today - $cache['today']) . '), R: ' . $cache['record'];
